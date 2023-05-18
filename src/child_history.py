0977b8812036bae4fa1eecfc45a409d7305705b8
auro.mohanty <aurogreenindia@gmail.com>
2023-05-18 18:37:22 +0200

    WIP: Get some history of children

--- src/child_history.py
+++ src/child_history.py
@@ -1,13 +1,27 @@
-import numpy as np
 from chromo.models import Pythia8
-from particle import Particle
 from visualize_event import pythia_config, run_conditions
 
 evt_kin = run_conditions()
-config = pythia_config("pythia_charm")
+config = pythia_config("pythia_charm") + pythia_config("pythia_d0_decay")
 pythia = Pythia8(evt_kin, seed=1, config=config)
 
 num_events = 10
+
+
+def child_status(event, parent_index_list):
+    # Get the child indices (subtract 1 from event-record indices)
+    # if len(parent_index_list) >= 1:
+    i = parent_index_list[0]
+    child_index_list = list(event.children[i] - 1)
+    # Filter as per child statuses
+    if event.children[i][0] == event.children[i][1] > 0:
+        child_index_list.remove(child_index_list[-1])
+    if event.children[i][0] > 0 and event.children[i][1] == 0:
+        child_index_list = []
+    if event.children[i][0] != event.children[i][1] and min(event.children[i]) > 0:
+        pass
+    return child_index_list
+
 
 for event in pythia(num_events):
     charm_chain = []  # start with the parent index
@@ -16,23 +30,22 @@ for event in pythia(num_events):
         # find the original outgoing parton
         if event.status[i] == 23 and abs(event.pid[i]) == 4:  # Charm quark PDG code
             charm_chain.append(i)
-        if len(charm_chain) == 0:
-            continue
-        if event.children[i] is None:
-            continue
-        child_index_list = list(event.children[i] - 1)
-        if event.children[i][0] == event.children[i][1]:
-            child_index_list.remove(child_index_list[-1])
-        charm_chain += child_index_list
 
-    for j in range(len(charm_chain)):
-        pid_chain.append(event.pid[charm_chain[j]])
-    if len(charm_chain) != 0:
-        print(
-            f"The event # is {event.nevent}. "
-            f"Daughter history of charm quark (particle ID: 4):\n{np.array(pid_chain)}"
-        )
-    if 421 in pid_chain:
-        print(f"There is {Particle.from_pdgid(421)} at {pid_chain.index(421)}")
-    if -421 in pid_chain:
-        print(f"There is {Particle.from_pdgid(-421)} at {pid_chain.index(-421)}")
+            j = 0
+            child_index_list = [i]
+            while j < 45:
+                child_index_list = child_status(event, child_index_list)
+                charm_chain += child_index_list
+                j += 1
+
+    # for j in range(len(charm_chain)):
+    #     pid_chain.append(event.pid[charm_chain[j]])
+    # if len(charm_chain) != 0:
+    #     print(
+    #         f"The event # is {event.nevent}. "
+    #         f"Daughter history of charm quark (particle ID: 4):\n{np.array(pid_chain)}"
+    #     )
+    # if 421 in pid_chain:
+    #     print(f"There is {Particle.from_pdgid(421)} at {pid_chain.index(421)}")
+    # if -421 in pid_chain:
+    #     print(f"There is {Particle.from_pdgid(-421)} at {pid_chain.index(-421)}")
